fed37e4549c32a70ee30df0695e99e1ac0c04a9d
felipecarvalho <felipe.carvalho@epadoca.com>
2025-01-12 16:38:35 -0300

    Lab02

--- Lab01-AppServices/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Lab01-AppServices/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41e4002eadc8e41754ebf75bc606439dfa0dad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4789e187914d2f1760e4e097a026055a80096a0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
