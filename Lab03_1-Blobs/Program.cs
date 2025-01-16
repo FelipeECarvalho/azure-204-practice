abe4ee8b84c7faa57c15aaaa66c6679075fcd5a7
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-01-16 17:15:44 -0300

    .

--- Lab03_1-Blobs/Program.cs
+++ Lab03_1-Blobs/Program.cs
@@ -1,18 +1,14 @@
 ﻿using Azure.Storage;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
-using System;
-using System.ComponentModel;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace BlobManager
 {
     public class Program
     {
-        private const string blobServiceEndpoint = "";
-        private const string storageAccountName = "";
-        private const string storageAccountKey = "";
+        private const string blobServiceEndpoint = "https://festorageaz204.blob.core.windows.net/";
+        private const string storageAccountName = "festorageaz204";
+        private const string storageAccountKey = "EjVu0+yziwli/SqCtVn4BS4G6a0Avf8nqJgBdcnH5AoMsymDnX52TuN5viwXEmcai06WFesGnoXl+AStTKmoqw==";
 
         public static async Task Main(string[] args)
         {
@@ -30,6 +26,9 @@ namespace BlobManager
             await Console.Out.WriteLineAsync($"Account sku:\t{info?.SkuName}");
 
             await EnumerateContainersAsync(serviceClient);
+
+            var newContainer = await GetContainerAsync(serviceClient, "vector-graphics");
+            var blobClient = await GetBlobAsync(newContainer, "graph.svg");
         }
 
         private static async Task EnumerateContainersAsync(BlobServiceClient client)
@@ -51,6 +50,30 @@ namespace BlobManager
             {
                 await Console.Out.WriteLineAsync($"Existing Blob:\t{blob.Name}");
             }
+        }
+
+        private static async Task<BlobContainerClient> GetContainerAsync(BlobServiceClient client, string containerName)
+        {
+            var container = client.GetBlobContainerClient(containerName);
+
+            await container.CreateIfNotExistsAsync();
+
+            await Console.Out.WriteLineAsync($"New Container:\t{container.Name}");
+
+            return container;
+        }
+
+        private static async Task<BlobClient> GetBlobAsync(BlobContainerClient client, string blobName)
+        {
+            var blobClient = client.GetBlobClient(blobName);
+            bool exist = await blobClient.ExistsAsync();
+
+            if (!exist)
+                await Console.Out.WriteLineAsync($"Blob {blobClient.Name} not found!");
+            else
+                await Console.Out.WriteLineAsync($"Blob Found, URI:\t{blobClient.Uri}");
+
+            return blobClient;
         }
     }
 }
