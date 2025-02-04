5f2b8553f221ddb3f00e4ea7e1a76e73be66015a
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-02-04 00:43:14 -0300

    Labs container

---
+++ Lab05-Containers/Program.cs
@@ -0,0 +1,21 @@
+﻿// Check if network is available
+using System.Text;
+
+if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+{
+    var sb = new StringBuilder();
+
+    string hostname = System.Net.Dns.GetHostName();
+    System.Net.IPHostEntry host = System.Net.Dns.GetHostEntry(hostname);
+
+    foreach(System.Net.IPAddress address in host.AddressList)
+    {
+        sb.Append(" " + address);
+    }
+
+    Console.WriteLine(sb.ToString());
+ }
+ else
+ {
+    Console.WriteLine("No Network Connection");
+ }
