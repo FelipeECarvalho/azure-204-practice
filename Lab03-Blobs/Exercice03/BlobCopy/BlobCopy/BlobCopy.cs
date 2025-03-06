f3ecee097fb45173ee40b5fb9c14b97d47e60806
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-03-06 15:50:30 -0300

    fix

---
+++ Lab03-Blobs/Exercice03/BlobCopy/BlobCopy/BlobCopy.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Storage.Blobs.Specialized;
+
+namespace BlobCopy
+{
+    static class BlobCopy
+    {
+        public static async Task Copy(string connectionString, string sourceContainer, string destContainer, string sourceFile, string destFile)
+        {
+            var sourceClient = new BlockBlobClient(connectionString, sourceContainer, sourceFile);
+            var destinationClient = new BlockBlobClient(connectionString, destContainer, destFile);
+
+            await destinationClient.StartCopyFromUriAsync(sourceClient.Uri);
+        }
+    }
+}
---
+++ Lab03-Blobs/Exercice03/BlobCopy/BlobCopy/Program.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+namespace BlobCopy
+{
+    internal class Program
+    {
+        private const string ConnectionString = "";
+
+        static async void Main(string[] args)
+        {
+            var sourceContainer = "firstcontainer";
+            var destContainer = "secondcontainer";
+            var sourceFile = "file1.txt";
+            var destFile = "file1_copy.txt";
+
+            await BlobCopy.Copy(ConnectionString, sourceContainer, destContainer, sourceFile, destFile);
+
+            var sourceClient = new BlockBlobClient(ConnectionString, sourceContainer, sourceFile);
+            BlobProperties properties = await sourceClient.GetPropertiesAsync();
+
+            Console.WriteLine("The tier of the source blob is: " + properties.AccessTier);
+            
+            var metaData = new Dictionary<string, string>
+            {
+                {"CreatedBy", "Felipe"},
+                {"CreateDate", DateTime.Now.ToShortDateString() }
+            };
+
+            await sourceClient.SetMetadataAsync(metaData);
+        }
+    }
+}
